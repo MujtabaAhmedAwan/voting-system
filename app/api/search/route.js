0e3981d27d92dcd2b242e22a9a9febbb85ce315e
Mujtaba Ahmed Awan <sultanmujtabaahmedawan@gmail.com>
2026-08-15 01:52:33 -0700

    Revert force-dynamic

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -2,8 +2,6 @@ import { NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
 
-export const dynamic = 'force-dynamic';
-
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const query = searchParams.get('q');
